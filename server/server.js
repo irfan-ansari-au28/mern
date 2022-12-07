1641b7e6e307049d004834a64dfb1962aeb84b12
apple <apple@apples-MacBook-Pro.local>
2022-12-08 01:07:55 +0530

    serve client index.html

--- server/server.js
+++ server/server.js
@@ -4,23 +4,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-// const config = require("./config/database");
-// const passport = require("passport");
-// const users = require("./routes/users");
+
 const port = process.env.PORT || 8080;
-
-// //connect to database
-// mongoose.connect(config.database);
-
-// //on connection
-// mongoose.connection.on("connected", () => {
-//   console.log("connected to database " + config.database);
-// });
-
-// //on error
-// mongoose.connection.on("error", (err) => {
-//   console.log("database error " + err);
-// });
 
 //cors middleware
 app.use(cors());
@@ -31,21 +16,13 @@ app.use(express.static(path.join(__dirname, "public")));
 //body parser middleware
 app.use(bodyParser.json());
 
-// //passport middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// require("./config/passport")(passport);
-
-// app.use("/users", users);
-
 //index route
 app.get("/", (req, res) => {
   res.send("Invalid Endpoint");
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "client/public/index.html"));
 });
 
 //start server
