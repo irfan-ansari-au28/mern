6746d1716fb1e3072e918734eeeb6d5e971a56c5
apple <apple@apples-MacBook-Pro.local>
2022-12-08 18:01:58 +0530

    create mongoose connection

--- server/server.js
+++ server/server.js
@@ -4,10 +4,47 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+// require("dotenv").config();
+
+const userModel = require("./models/User");
 
 const port = process.env.PORT || 8080;
 
-//cors middleware
+/* mangoose connection in express server */
+
+app.use(async (req, res, next) => {
+  try {
+    mongoose.connect(
+      process.env.MONGO_URI ||
+        "mongodb+srv://adminUser:admin@cluster0.qbie7i4.mongodb.net/mern?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+      }
+    );
+
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error: "));
+    db.on("connected", () => {
+      console.log("Mongoose is connected!!!!");
+    });
+
+    db.on("error", (err) => {
+      console.log(err, " mongoose failed to connect");
+    });
+    db.once("open", function () {
+      console.log("Connected successfully");
+    });
+
+    next();
+  } catch (error) {
+    console.log("Error occurred :", error);
+    process.exit(1);
+  }
+});
+
+// cors middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 //set static folder
@@ -20,6 +57,17 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Invalid Endpoint");
 });
+// app.post("/add_user", async (request, response) => {
+//   const user = new userModel(request.body);
+
+//   try {
+//     await user.save();
+//     console.log("User added ");
+//     response.send(user);
+//   } catch (error) {
+//     response.status(500).send(error);
+//   }
+// });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/public/index.html"));
@@ -27,5 +75,7 @@ app.get("*", (req, res) => {
 
 //start server
 app.listen(port, () => {
+  console.log("------------------------------");
   console.log("server started on port " + port);
+  console.log("------------------------------");
 });
